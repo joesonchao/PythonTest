3abadc8108f140f9d626b5e85564576af97409c3
Joeson <joesonchao@gmail.com>
2019-05-27 11:59:59 +0800

    Show Version

---
+++ demo1.py
@@ -0,0 +1,7 @@
+import sys
+import numpy, sklearn
+
+print("Python Version {}".format(sys.version))
+print("numpy version:{}".format(numpy.__version__))
+print("sklearn version:{}".format(sklearn.__version__))
+
