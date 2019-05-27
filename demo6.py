c9fbd1fc48919f79a5660ae9375566650b67e620
Joeson <joesonchao@gmail.com>
2019-05-27 16:37:22 +0800

    demo6

--- demo6.py
+++ demo6.py
@@ -1,6 +1,11 @@
-import  matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn import linear_model, datasets
 
-regressionData1 = datasets.make_regression(10,5)
+regressionData1 = datasets.make_regression(100, 1, noise=10)
+#noise會修改畫出來的數值
 print(type(regressionData1))
-print(regressionData1[0].shap, regressionData1[1].shape)
+print(regressionData1[0].shape, regressionData1[1].shape)
+regression1 = linear_model.LinearRegression()
+regression1.fit(regressionData1[0], regressionData1[1])
+print("coef={}, intercept={}".format((regression1.coef_, regression1.intercept_)))
+print("score={}".format((regression1.score(()))))
