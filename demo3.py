14a5facfbbf0154ca2c957c369bcb5a4e39d39f7
Joeson <joesonchao@gmail.com>
2019-05-27 15:55:13 +0800

    demo4

--- demo3.py
+++ demo3.py
@@ -6,11 +6,11 @@ features = [[1], [2], [3]]
 values = [1, 4, 9]
 plt.scatter(features, values, c='green')
 plt.show()
-regression1.fit(feature, values)
+regression1.fit(features, values)
 # y=ax+b, a==> coef, b==>intercept
 print('coefficient={}'.format(regression1.coef_))
 print('intercept={}'.format(regression1.intercept_))
 range1 = [0, 3]
 plt.plot(range1, regression1.coef_ * range1 + regression1.intercept_, c='gray')
-print('score='c, regression1.score(features, values))
+print('score={}', regression1.score(features, values))
 plt.show()
