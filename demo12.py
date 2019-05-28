8a1a58805cfddeae420e29baab898792aa40a0fa
Joeson <joesonchao@gmail.com>
2019-05-28 10:35:59 +0800

    demo12

---
+++ demo12.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+iris = datasets.load_iris()
+print(type(iris))
+
+X = iris.data
+labels = ['sepal length', 'sepal width', 'peta length', 'peta width']
+species = iris.target
+print(type(X), type(species))
+
+counter = 1
+for i in range(0, 4):
+    for j in range(i + 1, 4):
+        plt.figure(counter, figsize=(8, 6))
+        counter + 1
+        xData = X[:, i]
+        yData = X[:, j]
+        x_min, x_max = xData.min() - 0.5, xData.max() + 0.5
+        y_min, y_max = yData.min() - 0.5, yData.max() + 0.5
+
+        plt.clf()
+        plt.scatter(xData, yData, c=species, cmap=plt.cm.Paired)
+        plt.show()
