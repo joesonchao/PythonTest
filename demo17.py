675d59f2ac65a2546e8e830c157f980a6f110ac0
Joeson <joesonchao@gmail.com>
2019-05-28 13:40:12 +0800

    demo 17

--- demo17.py
+++ demo17.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+mport matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, svm
 from sklearn.decomposition import PCA
@@ -9,5 +9,20 @@ pca = PCA(n_components=2)
 
 data = pca.fit(iris.data).transform(iris.data)
 
+# 顯示資料維度
 print(data.shape)
 print(iris.data.shape)
+
+datamax = data.max(axis=0) + 1
+datamin = data.min(axis=0) - 1
+print(datamax)
+print(datamin)
+n = 2000
+X, Y = np.meshgrid(np.linspace(datamin[0], datamax[0], n),
+                   np.linspace(datamin[1], datamax[1], n))
+svc = svm.SVC()
+svc.fit(data, iris.target)
+Z = svc.predict(np.c_[X.ravel(),Y.ravel()])
+
+plt.contour(X,Y,Z.reshape(X.shape), colors='K')
+plt.show()
