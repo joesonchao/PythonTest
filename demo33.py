5c4a80a09fce976da5033a9905c82e96aa1fc6ae
Joeson <joesonchao@gmail.com>
2019-05-29 14:25:03 +0800

    demo33

--- demo33.py
+++ demo33.py
@@ -2,6 +2,32 @@ from sklearn import datasets
 from sklearn.decomposition import PCA
 
 iris = datasets.load_iris()
-
 pca = PCA(n_components=2)
 data = pca.fit(iris.data).transform(iris.data)
+
+datamax = data.max(axis=0) + 1
+datamin = data.min(axis=0) - 1
+print(datamax, datamin)
+import numpy as np
+from sklearn import svm
+import matplotlib.pyplot as plt
+
+n = 4000
+X, Y = np.meshgrid(np.linspace(datamin[0], datamax[0], n),
+                   np.linspace(datamin[1], datamax[1], n))
+svc = svm.SVC(C=2000)
+svc.fit(data, iris.target)
+Z = svc.predict(np.c_[X.ravel(), Y.ravel()])
+
+plt.contour(X, Y, Z.reshape(X.shape), levels=[-0.5, 0.5, 1.5, 2.5], colors=['r', 'g', 'b', 'y'])
+
+for i, c in zip([0, 1, 2], ['r', 'g', 'b']):
+    d = data[iris.target == i]
+    plt.scatter(d[:, 0], d[:, 1], c=c)
+plt.show()
+
+plt.show()
+
+data = pca.fit(iris.data).transform(iris.data)
+
+print(data)
