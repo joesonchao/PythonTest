4eae5d9ce56a6790d0205442b025cf28645ffb82
Joeson <joesonchao@gmail.com>
2019-05-31 14:17:35 +0800

    update to 10 digits

--- digit.py
+++ digit.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 # 載入 `digits`
 digits = datasets.load_digits()
+print(type(digits))
 
 # 設定圖形的大小（寬, 高）
 fig = plt.figure(figsize=(4, 2))
@@ -12,14 +13,14 @@ fig = plt.figure(figsize=(4, 2))
 # 調整子圖形
 fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
 
-# 把前 8 個手寫數字顯示在子圖形
-for i in range(8):
-    # 在 2 x 4 網格中第 i + 1 個位置繪製子圖形，並且關掉座標軸刻度
-    ax = fig.add_subplot(2, 4, i + 1, xticks = [], yticks = [])
+# 把 10 個手寫數字顯示在子圖形
+for i in range(10):
+    # 在 2 x 5 網格中第 i + 1 個位置繪製子圖形，並且關掉座標軸刻度
+    ax = fig.add_subplot(2, 5, i + 1, xticks=[], yticks=[])
     # 顯示圖形，色彩選擇灰階
-    ax.imshow(digits.images[i], cmap = plt.cm.binary)
+    ax.imshow(digits.images[i], cmap=plt.cm.binary)
     # 在左下角標示目標值
-    ax.text(0, 7, str(digits.target[i]))
+    ax.text(0, 9, str(digits.target[i]))
 
 # 顯示圖形
 plt.show()
